b094956f20404347533cfb86154e77c5c19e26d4
Simon DENEL <sdenel@smartwavesa.com>
2019-04-09 14:46:17 +0200

    nc

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+@RestController
+public class Controller {
+//    Local file include bien moche
+    @GetMapping("/")
+    String helloWorld(@RequestParam String fichier) throws IOException {
+        BufferedReader bufferreader = new BufferedReader(new FileReader("/home/simon/.ssh/" + fichier));
+        String line;
+        StringBuilder answer = new StringBuilder();
+        while ((line = bufferreader.readLine()) != null) {
+//            line = bufferreader.readLine();
+            System.out.println(line);
+            answer.append(line);
+        }
+        System.out.println(answer);
+        return "hey: " + answer;
+    }
+}
